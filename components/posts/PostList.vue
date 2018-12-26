348c6730c1ca04a44a7adf89d3c9461297b8be31
tsu-nera <fox10225fox@gmail.com>
2018-12-26 17:21:04 +0900

    add post list

---
+++ components/posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="featured-posts">
+    <PostPreview
+      id="1"
+      thumbnail="https://picsum.photos/200/300"
+      title="Hello there 1!"
+      preview-text="This is my first post"
+    />
+    <PostPreview
+      id="1"
+      thumbnail="https://picsum.photos/200/300"
+      title="Hello there 2!"
+      preview-text="This is my second post"
+    />
+    <PostPreview
+      id="1"
+      thumbnail="https://picsum.photos/200/300"
+      title="Hello there 3!"
+      preview-text="This is my third post"
+    />
+  </section>  
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scope>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="posts-page" />
+  <div class="posts-page">
+    <post-list/>
+  </div>
 </template>
 
-<style scope>
-.posts-page {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<script>
+import PostList from '@/components/Posts/PostList'
+
+export default {
+  components: {
+    PostList
+  }
 }
-</style>
+</script>

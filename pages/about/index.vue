3fac3cb3650ed2724b8a5750b8eb041aecc9b69c
tsu-nera <fox10225fox@gmail.com>
2018-12-26 16:12:17 +0900

    add about post admin

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>About</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur harum nostrum. Iure dicta possimus explicabo, ut vero quis sint, repellendus earum laudantium provident in ducimus et eum ea laboriosam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur harum nostrum. Iure dicta possimus explicabo, ut vero quis sint, repellendus earum laudantium provident in ducimus et eum ea laboriosam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur harum nostrum. Iure dicta possimus explicabo, ut vero quis sint, repellendus earum laudantium provident in ducimus et eum ea laboriosam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora tenetur harum nostrum. Iure dicta possimus explicabo, ut vero quis sint, repellendus earum laudantium provident in ducimus et eum ea laboriosam?</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="posts-page" />
+</template>
+
+<style scope>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
